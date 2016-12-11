5f2e788a8e18aed96db1b9807617563979535d43
cmdalbem <ninja.dalbem@gmail.com>
2016-12-11 14:41:32 -0200

    removes collapsable effects in documentation views

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,18 +1,18 @@
 (function($){
   'use strict';
 
-  var $endpointsLists = $('ul.endpoints');
-  $endpointsLists.hide();
+  // var $endpointsLists = $('ul.endpoints');
+  // $endpointsLists.hide();
 
-  $('.api-item-title').on('click', function(event) {
-    var $apiItem = $(this).parent('.api-item').find('ul.endpoints');
-    $endpointsLists.not($apiItem).hide("fast");
+  // $('.api-item-title').on('click', function(event) {
+  //   var $apiItem = $(this).parent('.api-item').find('ul.endpoints');
+  //   $endpointsLists.not($apiItem).hide("fast");
 
-    if ($apiItem.is(':visible')) {
-      $apiItem.hide("fast");
-    } else {
-      $apiItem.show("fast");
-    }
-  });
+  //   if ($apiItem.is(':visible')) {
+  //     $apiItem.hide("fast");
+  //   } else {
+  //     $apiItem.show("fast");
+  //   }
+  // });
 
 }(jQuery));
