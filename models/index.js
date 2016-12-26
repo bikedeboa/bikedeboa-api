1155c9e4c74d03409e300685a319ee8f64dc629c
Denner Evaldt <dnrevaldt@gmail.com>
2016-12-26 09:42:14 -0200

    fixes

--- models/index.js
+++ models/index.js
@@ -11,7 +11,6 @@ var db        = {};
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  console.log(config);
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
