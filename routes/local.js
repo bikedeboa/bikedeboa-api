76e2a9b5de6c4a085d9c054a665636e099f456ea
Denner Evaldt <dnrevaldt@gmail.com>
2017-01-23 09:58:47 -0200

    get all light fix

--- routes/local.js
+++ routes/local.js
@@ -6,16 +6,12 @@ var models = require('../models');
 var LocalController = require('../controllers/LocalController')(models.Local);
 var AuthController = require('../controllers/AuthController')(models.User);
 
-router.use(AuthController.middlewareAuth);
-router.use(acl.authorize);
-router.use(AuthController.middlewareLogging);
-
-router.get('/', LocalController.getAll.bind(LocalController));
+router.get('/', AuthController.middlewareAuth, acl.authorize, AuthController.middlewareLogging, LocalController.getAll.bind(LocalController));
 router.get('/light', LocalController.getAllLight.bind(LocalController));
-router.get('/:_id', LocalController.getById.bind(LocalController));
-router.post('/', LocalController.create.bind(LocalController));
-router.put('/:_id', AuthController.middlewareValidIP, LocalController.update.bind(LocalController));
-router.delete('/:_id', AuthController.middlewareValidIP, LocalController.remove.bind(LocalController));
-router.delete('/', LocalController.removeAll.bind(LocalController));
+router.get('/:_id', AuthController.middlewareAuth, acl.authorize, AuthController.middlewareLogging, LocalController.getById.bind(LocalController));
+router.post('/', AuthController.middlewareAuth, acl.authorize, AuthController.middlewareLogging, LocalController.create.bind(LocalController));
+router.put('/:_id', AuthController.middlewareAuth, acl.authorize, AuthController.middlewareLogging, AuthController.middlewareValidIP, LocalController.update.bind(LocalController));
+router.delete('/:_id', AuthController.middlewareAuth, acl.authorize, AuthController.middlewareValidIP, AuthController.middlewareLogging, LocalController.remove.bind(LocalController));
+router.delete('/', AuthController.middlewareAuth, acl.authorize, AuthController.middlewareLogging, LocalController.removeAll.bind(LocalController));
 
 module.exports = router;
