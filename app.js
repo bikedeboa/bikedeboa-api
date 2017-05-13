4467ce268703fa4e417fc48d317f592d320119ba
cmdalbem <ninja.dalbem@gmail.com>
2017-05-13 14:46:58 -0300

    now it goes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let express = require('express')
 let methodOverride = require('method-override')
 let bodyParser = require('body-parser')
+let cors = require('cors')
 let app = express()
 let path = require('path')
 let swig = require('swig')
@@ -16,6 +17,11 @@ app.use(methodOverride('_method'))
 app.use(bodyParser.json({limit: '50mb'}))
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
 app.use(helmet())
+app.use(cors({
+  origin: [true],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'x-access-token', 'ip_origin']
+}))
 app.use(compression())
 
 // static
