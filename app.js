594a5c8f2c4e56ecb6a30c31e4401acfcc96eb28
Cristiano Medeiros Dalbem <cristiano.dalbem@gmail.com>
2017-07-10 00:31:02 -0300

    allow CORS for new front-end development origin

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json({limit: '50mb'}))
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
 app.use(helmet())
 app.use(cors({
-  origin: ['https://www.bikedeboa.com.br'],
+  origin: ['https://www.bikedeboa.com.br','https://bikedeboa-dev.herokuapp.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'x-access-token', 'ip_origin']
 }))
