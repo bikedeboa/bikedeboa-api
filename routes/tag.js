650ad7b161ce55bcfefef35232ebcb92e6775266
Cristiano M Dalbem <cristiano.dalbem@vtex.com.br>
2017-11-27 21:32:43 -0200

    getAllTags endpoint not passing by ACL anymore

--- routes/tag.js
+++ routes/tag.js
@@ -5,13 +5,13 @@ let models = require('../models')
 let TagController = require('../controllers/TagController')(models.Tag)
 let AuthController = require('../controllers/AuthController')(models.User)
 
-router.use(AuthController.middlewareAuth)
-router.use(acl.authorize)
+// router.use(AuthController.middlewareAuth)
+// router.use(acl.authorize)
 
 router.get('/', TagController.getAll.bind(TagController))
-router.get('/:_id', TagController.getById.bind(TagController))
-router.post('/', AuthController.middlewareLogging, TagController.create.bind(TagController))
-router.put('/:_id', AuthController.middlewareLogging, TagController.update.bind(TagController))
-router.delete('/:_id', AuthController.middlewareLogging, TagController.remove.bind(TagController))
+router.get('/:_id', AuthController.middlewareAuth, acl.authorize, TagController.getById.bind(TagController))
+router.post('/', AuthController.middlewareAuth, acl.authorize, AuthController.middlewareLogging, TagController.create.bind(TagController))
+router.put('/:_id', AuthController.middlewareAuth, acl.authorize, AuthController.middlewareLogging, TagController.update.bind(TagController))
+router.delete('/:_id', AuthController.middlewareAuth, acl.authorize, AuthController.middlewareLogging, TagController.remove.bind(TagController))
 
 module.exports = router
