5ea421eb170b44046bb32739a2573df228bbe1a8
Denner Evaldt <dnrevaldt@gmail.com>
2017-12-02 08:49:13 -0200

    create pagination log

--- controllers/LogController.js
+++ controllers/LogController.js
@@ -3,13 +3,26 @@ function LogController (LogModel) {
 }
 
 LogController.prototype.getAll = function (request, response, next) {
-  var _query = {}
+  let limit = 100; // number of records per page
+  let offset = 0;
 
-  this.model.findAll(_query)
-    .then(function (data) {
-      response.json(data)
+  this.model.findAndCountAll()
+  .then((data) => {
+    let page = request.params.page; // page number
+    let pages = Math.ceil(data.count / limit);
+		offset = limit * (page - 1);
+    this.model.findAll({
+      limit: limit,
+      offset: offset,
+      $sort: { id: 1 }
     })
-    .catch(next)
+    .then((logs) => {
+      response.json({'result': logs, 'count': data.count, 'pages': pages});
+    });
+  })
+  .catch(function (error) {
+		next(error)
+	});
 }
 
 module.exports = function (LogModel) {
