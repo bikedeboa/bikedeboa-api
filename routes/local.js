b4f0a18cf99bd57df78e787a2d01e7a4710b9c12
Cristiano Dalbem <ninja.dalbem@gmail.com>
2018-06-02 22:08:28 -0300

    Remove legacy Auth Middleware based on IP

--- routes/local.js
+++ routes/local.js
@@ -9,7 +9,7 @@ router.get('/', AuthController.middlewareAuth, acl.authorize, LocalController.ge
 router.get('/light', LocalController.getAllLight.bind(LocalController))
 router.get('/:_id', LocalController.getById.bind(LocalController))
 router.post('/', AuthController.middlewareAuth, acl.authorize, AuthController.middlewareLogging, LocalController.create.bind(LocalController))
-router.put('/:_id', AuthController.middlewareAuth, acl.authorize, AuthController.middlewareLogging, AuthController.middlewareValidIP, LocalController.update.bind(LocalController))
-router.delete('/:_id', AuthController.middlewareAuth, acl.authorize, AuthController.middlewareLogging, AuthController.middlewareValidIP, LocalController.remove.bind(LocalController))
+router.put('/:_id', AuthController.middlewareAuth, acl.authorize, AuthController.middlewareLogging, LocalController.update.bind(LocalController))
+router.delete('/:_id', AuthController.middlewareAuth, acl.authorize, AuthController.middlewareLogging, LocalController.remove.bind(LocalController))
 
 module.exports = router
