ba808585b628899a57aa3ec07ec5d6e0d7f286e3
Tadeu Saldanha <tadeusaldanha@gmail.com>
2019-02-23 18:28:48 -0300

    Fix rout

--- routes/support.js
+++ routes/support.js
@@ -8,9 +8,6 @@ let AuthController = require('../controllers/AuthController')(models.User)
 router.use(AuthController.middlewareAuth)
 router.use(acl.authorize)
 
-router.get('/', SupportController.getAll.bind(SupportController))
-router.get('/:_id', SupportController.getById.bind(SupportController))
-router.put('/:_id', SupportController.update.bind(SupportController))
 router.delete('/:_id', AuthController.middlewareLogging, SupportController.remove.bind(SupportController))
 router.post('/', AuthController.middlewareLogging, SupportController.create.bind(SupportController))
 
